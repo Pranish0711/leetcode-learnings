303c504223389200f4e968eec793418891fba01e
Pranish1507 <praoje@gmail.com>
2023-04-20 14:47:38 +0530

    Solved BuildArrayFromPermutation problem

---
+++ src/com/learnings/leetcode/arrays/BuildArrayFromPermutation.java
@@ -0,0 +1,24 @@
+package com.learnings.leetcode.arrays;
+
+import java.util.Arrays;
+
+// https://leetcode.com/problems/build-array-from-permutation/description/
+
+public class BuildArrayFromPermutation {
+
+  public static void main(String[] args) {
+    System.out.println("hello");
+    int[] inp = new int[] { 5,0,1,2,3,4 };
+    BuildArrayFromPermutation obj = new BuildArrayFromPermutation();
+    System.out.println(Arrays.toString(obj.buildArray(inp)));
+
+  }
+
+  public int[] buildArray(int[] nums) {
+    int[] ans = new int[nums.length];
+    for (int i = 0; i < nums.length; i++) {
+      ans[i] = nums[nums[i]];
+    }
+    return ans;
+  }
+}
